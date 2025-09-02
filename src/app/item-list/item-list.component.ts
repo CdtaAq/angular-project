6f3df800d18643440d22d347c26b0bbcf32394f5
chris Taylor <ctay456a@gmail.com>
2025-09-02 15:02:02 -0400

    Create item-list.component.ts

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent {
+  items: string[] = ['Apple', 'Banana', 'Orange'];
+  newItem: string = '';
+
+  addItem() {
+    if (this.newItem.trim()) {
+      this.items.push(this.newItem.trim());
+      this.newItem = '';
+    }
+  }
+}
